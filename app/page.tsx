bac6e2ca8f1dd8be025706a4af06a4b06f0c6d6b
ngtankhoa <40681049+ngtankhoa@users.noreply.github.com>
2024-09-16 10:31:48 +0700

    temp

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,7 @@ import NetworkCard from './ui/network-card';
 function urlBase64ToUint8Array(base64String: string) {
   console.log(base64String);
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
-  const base64 = (base64String + padding)
-    .replace(/-/g, '+')
-    .replace(/_/g, '/');
+  const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
 
   const rawData = window.atob(base64);
   const outputArray = new Uint8Array(rawData.length);
@@ -29,6 +27,7 @@ function InstallPrompt() {
 
   useEffect(() => {
     setIsIOS(
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       /iPad|iPhone|iPod/.test(navigator.userAgent) && !(window as any).MSStream,
     );
 
