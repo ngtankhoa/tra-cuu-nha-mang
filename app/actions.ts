1b1a098b5fa0f29a4fd67c0dd17783ec5c469ffc
ngtankhoa <40681049+ngtankhoa@users.noreply.github.com>
2024-09-16 10:29:19 +0700

    fix

--- app/actions.ts
+++ app/actions.ts
@@ -3,7 +3,7 @@
 import webpush from 'web-push';
 
 webpush.setVapidDetails(
-  '<mailto:khoant.uit@gmail.com>',
+  'mailto:khoant.uit@gmail.com',
   process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY!,
   process.env.VAPID_PRIVATE_KEY!,
 );
--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,10 @@ import { networks } from './lib/data';
 import NetworkCard from './ui/network-card';
 
 function urlBase64ToUint8Array(base64String: string) {
+  console.log(base64String);
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
   const base64 = (base64String + padding)
-    .replace(/\\-/g, '+')
+    .replace(/-/g, '+')
     .replace(/_/g, '/');
 
   const rawData = window.atob(base64);
